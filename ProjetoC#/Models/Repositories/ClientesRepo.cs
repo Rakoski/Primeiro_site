b882cdcfdccb2556a759e1069a76b0190d933037
mastr <mastrakoski@gmail.com>
2023-06-06 23:23:47 -0300

    Added "delete" feature
    Now, users can delete anyone they want from the website if they choose to do so

--- ProjetoC#/Models/Repositories/ClientesRepo.cs
+++ ProjetoC#/Models/Repositories/ClientesRepo.cs
@@ -4,6 +4,7 @@ using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
 using MySql.Data.MySqlClient;
 using System.Data;
+using MySqlX.XDevAPI;
 
 namespace ProjetoC_.Models.Repositories
 {
@@ -11,8 +12,8 @@ namespace ProjetoC_.Models.Repositories
     public class ClientesRepo
     {
 
-        // convertendo as coisas do site em formato de json no arquivo .txt abaixo
         public void Salvar(Clientes clientes)
+        
         {
 
             string server = "localhost";
@@ -22,71 +23,60 @@ namespace ProjetoC_.Models.Repositories
             string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" +
                 "UID=" + username + ";" + "PASSWORD=" + password + ";";
 
-            // mano, pega essa lista desse arquivo aqui
-            var tipo_da_variavel = File.ReadAllText("C:\\Users\\mastr\\OneDrive\\Área de Trabalho\\c#\\CadastroClientes\\Banco\\banco_dados.txt");
+            List<Clientes> clientesList = new List<Clientes> { clientes };
 
-            // e coloca ela dentro dessa lista (lista Clientes criada antes)
-            List<Clientes>? data = JsonConvert.DeserializeObject<List<Clientes>>("[" + tipo_da_variavel + "]");
-
-            // Primeiro eu tenho que conectar com o MySQL workbench
+            // First, I have to connect to MySQL workbench
             using (MySqlConnection conn = new MySqlConnection(constring))
             {
                 conn.Open();
 
-                // Depois eu cologo, pra cada item nos dados (data), sendo que os dados é o deserialized object.
-                // I'm running into an issue here. The website is saving the already listed data from Listar(), not
-                // the new saved cadastro when I try to save another person
-                foreach (var item in data)
+                foreach (var cliente in clientesList)
                 {
-
-                    string query = "INSERT INTO clientes (Documento, nome, sexo, email, telefone, fax, UF) " +
-                                   "VALUES (@Documento, @Nome, @Sexo, @Email, @Telefone, @Fax, @UF)";
-
-                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    // Checking if the cliente already exists in the database based on the Documento field
+                    string query = "SELECT COUNT(*) FROM clientes WHERE Documento = @Documento";
+                    using (MySqlCommand checkCmd = new MySqlCommand(query, conn))
                     {
-                        // Set the parameter values
-                        cmd.Parameters.AddWithValue("@Documento", item.Documento);
-                        cmd.Parameters.AddWithValue("@Nome", item.Nome);
-                        cmd.Parameters.AddWithValue("@Sexo", item.Sexo);
-                        cmd.Parameters.AddWithValue("@Email", item.Email);
-                        cmd.Parameters.AddWithValue("@Telefone", item.Telefone);
-                        cmd.Parameters.AddWithValue("@Fax", item.Fax);
-                        cmd.Parameters.AddWithValue("@UF", item.UF);
+                        checkCmd.Parameters.AddWithValue("@Documento", cliente.Documento);
+                        int existingCount = Convert.ToInt32(checkCmd.ExecuteScalar());
 
-                        // Execute the query
-                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (existingCount > 0)
+                        {
+                            // If the cliente exists, delete the existing record
+                            query = "DELETE FROM clientes WHERE Documento = @Documento";
+                            using (MySqlCommand deleteCmd = new MySqlCommand(query, conn))
+                            {
+                                deleteCmd.Parameters.AddWithValue("@Documento", cliente.Documento);
+                                deleteCmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
 
-                        if (rowsAffected > 0)
-                        {
-                            Console.WriteLine("Data inserted successfully!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Failed to insert data.");
-                        }
+                    // Insert the cliente record into the database
+                    query = "INSERT INTO clientes (Documento, nome, sexo, email, telefone, fax, UF) " +
+                            "VALUES (@Documento, @Nome, @Sexo, @Email, @Telefone, @Fax, @UF)";
+                    using (MySqlCommand insertCmd = new MySqlCommand(query, conn))
+                    {
+                        insertCmd.Parameters.AddWithValue("@Documento", cliente.Documento);
+                        insertCmd.Parameters.AddWithValue("@Nome", cliente.Nome);
+                        insertCmd.Parameters.AddWithValue("@Sexo", cliente.Sexo);
+                        insertCmd.Parameters.AddWithValue("@Email", cliente.Email);
+                        insertCmd.Parameters.AddWithValue("@Telefone", cliente.Telefone);
+                        insertCmd.Parameters.AddWithValue("@Fax", cliente.Fax);
+                        insertCmd.Parameters.AddWithValue("@UF", cliente.UF);
+                        insertCmd.ExecuteNonQuery();
                     }
                 }
             }
 
-            // Vai listar tudo do banco de dados
-            // var ClientesLista = Listar();
-            // vai achar ele
-            // var item = ClientesLista.Where(a => a.Documento == clientes.Documento).FirstOrDefault();
-
-            // Se ele existe eu deleto
-            //if (item != null)
-            //{
-            //Deletar(clientes.Documento);
-            //}
-
-            // se ele não existir, já vai salvar de qualquer jeito mesmo
-            // string retorno_json = JsonConvert.SerializeObject(clientes, Formatting.Indented) + " ," + Environment.NewLine;
-
-            // File.AppendAllText(@"C:\\Users\\mastr\\OneDrive\\Área de Trabalho\\c#\\CadastroClientes\\Banco\\banco_dados.txt", retorno_json);
+            Console.WriteLine("Data transfer completed.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
 
+
         // Here, I'll make a function that only retrieves the data so that I can list it in the Website using MySQL
-        public List<Clientes> Listar() {
+        public List<Clientes> Listar()
+        {
 
             string server = "localhost";
             string database = "cadastro_cliente";
@@ -133,36 +123,34 @@ namespace ProjetoC_.Models.Repositories
             }
         }
 
-        
-        public bool Deletar(string Document)
+        public bool Deletar(string Documento)
         {
-            var lista_clientes_document = Listar();
-            var item_encontrado = lista_clientes_document.Where(a=>a.Documento == Document).FirstOrDefault();
-            // Encontrando a variável no arquivo .txt para ser deletada depois. FirstOrDefault() significa o primeiro que encontrar é pra trazer
+            string server = "localhost";
+            string database = "cadastro_cliente";
+            string username = "root";
+            string password = "";
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" +
+                            "UID=" + username + ";" + "PASSWORD=" + password + ";";
 
-            if (item_encontrado != null)
+            var conexao = new MySqlConnection(constring);
+            conexao.Open();
+
+            var query = "DELETE FROM clientes WHERE documento = @Documento";
+            var comando = new MySqlCommand(query, conexao);
+            comando.Parameters.AddWithValue("@Documento", Documento);
+
+            var linhasAfetadas = comando.ExecuteNonQuery();
+
+            conexao.Close();
+
+            if (linhasAfetadas > 0)
             {
-
-                lista_clientes_document.Remove(item_encontrado);
-                // Remove e deixa o arquivo vazio (String.Empty)
-                File.WriteAllText("C:\\Users\\mastr\\OneDrive\\Área de Trabalho\\c#\\CadastroClientes\\Banco\\banco_dados.txt", string.Empty);
-
-                // Agora, temos que escrever tudo de novo, só que sem o item_encontrado que foi excluido
-                // foreach é a mesma coisa que um for em python, percorrendo uma lista, item por item
-                foreach (var cliente in lista_clientes_document)
-                {
-
-                    Salvar(cliente);
-
-                }
-
-                // Isso é basicamente um alert("true")
                 return true;
             }
 
             return false;
-
         }
+
 
         // Lembrando que ainda precisamos ter a função de editar o tal "banco de dados" que no momento é só um .txt 
         // Agora, nós temos que ir até o banco, achar o cliente que é pra editar e Pegar o Cliente
