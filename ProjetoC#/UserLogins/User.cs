2f9d191b1fcb38d9b024c0251e213a36ec4e6e25
mastr <mastrakoski@gmail.com>
2023-06-20 12:18:06 -0300

    Log-in/registration system made
    Now, users can log-in if their passwords match the encrypted ones in the db, if they do, they are sent a "token" message, if they don't, they are returned a badrequest

---
+++ ProjetoC#/UserLogins/User.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoC_.UserLogins;
+
+namespace ProjetoC_.UserLogins
+{
+    public class User
+    {
+        public int id_user { get; set; }
+
+        public string email { get; set; } = String.Empty!;
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
