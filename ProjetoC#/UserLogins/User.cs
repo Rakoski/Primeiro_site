fa7f1d52b149322ad47cb374bf4bd4cf28194165
mastr <mastrakoski@gmail.com>
2023-06-20 12:21:04 -0300

    Registration/Login user system complete
    The back-end of the user registration process is complete, if the user inserts the correct password, it is hashed and inserted into the database, if they match, a "token" is returned, else, a badrequest is sent

--- ProjetoC#/UserLogins/User.cs
+++ ProjetoC#/UserLogins/User.cs
@@ -2,11 +2,14 @@
 
 namespace ProjetoC_.UserLogins
 {
+
+    // Ok so I willnot store these passwords in plain text, which means I got to hash and salt them first
     public class User
     {
         public int id_user { get; set; }
 
         public string email { get; set; } = String.Empty!;
+
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
